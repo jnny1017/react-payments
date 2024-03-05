a755b1ff9e5a171b2521e07ef48a4d8a5374b53e
jnny1017 <jnny1017@gmail.com>
2024-03-05 15:39:16 +0900

    feat: 카드를 확인할 수 있는 CardPreview 컴포넌트 생성

---
+++ src/components/CardPreview.tsx
@@ -0,0 +1,36 @@
+import { FormValuesType } from '../model/FormValuesType';
+import formatCardNumber from '../utils/utils';
+
+interface Props {
+    values: FormValuesType;
+}
+
+export default function CardPreview({ values }: Props) {
+    const {
+        cardNumber,
+        ownerName,
+        expiryDate: { expiryMonth, expiryYear },
+    } = values;
+
+
+    return (
+        <div className="card-box">
+            <div className="empty-card">
+                <div className="card-top"></div>
+                <div className="card-middle">
+                    <div className="small-card__chip"></div>
+                </div>
+                <div className="card-bottom">
+                    <div className="card-bottom__number">
+                    </div>
+                    <div className="card-bottom__info">
+                        <span className="card-text">{ownerName || 'NAME'}</span>
+                        <span className="card-text">
+                            {expiryMonth || 'MM'} / {expiryYear || 'YY'}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
